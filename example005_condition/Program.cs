a73b099f1c5b15dc371e1efe6217918583e23d7d
Mona Lisa <origami.nova@gmail.com>
2023-01-08 21:47:46 +0100

    ex5

---
+++ example005_condition/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter your name");
+string username = Console.ReadLine();
+
+if(username == "alex")
+{
+    Console.WriteLine("wow thats alex!,");
+}
+else
+{
+    Console.Write("hi,");
+    Console.WriteLine(username);
+
+}
